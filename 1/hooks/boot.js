bd7cdb958cd66b71f6b71642475c1f564b9e4094
MrRen <543169072@qq.com>
2020-11-20 14:17:27 +0800

    mrren

--- 1/hooks/boot.js
+++ 1/hooks/boot.js
@@ -1,4 +1,5 @@
+import   '../app.css'
 export default   ()=>{
-    console.log("boot");
+    
      
 }
--- 3/hooks/boot.js
+++ 3/hooks/boot.js
@@ -1,16 +1,27 @@
 import Vue  from "vue";
 import yitiji_ui from 'eapp-vue-yitiji'
-
+import packagejson from '../package.json'
+import   '../app.css'
 export default  async ()=>{
     Eapp.initialize({
         vue: Vue
     });
  
     yitiji_ui(Eapp);
-
-    Vue.component('epii-loading', epiiLoading)
- 
     Eapp.config = APP_CONFIG;
+    async function load(key) {
+        return new Promise(ok => {
+            import("../node_modules/" + key + "/" + key.replace("vue-ui-", "") + ".vue").then(m => {
+                Vue.component(key.replace("vue-ui-", ""), m.default);
+                ok();
+            });
+        });
+    }
+    for (let key in packagejson.dependencies) {
+        if (key.indexOf("epii-vue-ui-") === 0) {
+            await load(key);
+        }
+    }
   
    if (Eapp.config&& Eapp.config.hasOwnProperty("api_url_base")) {
        Eapp.http.setApiBase(Eapp.config.api_url_base)
